b7ca2ea886a9d50ed0412ebf4bb89c8e0504d004
Luki Kos <lukikos@users.noreply.github.com>
2026-04-20 03:02:34 +0800

    feat: add main entry point with version flag and config loading

--- main.go
+++ main.go
@@ -1,11 +1,44 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"log"
 	"os"
 
-	"github.com/glanceapp/glance/internal/glance"
+	"github.com/glanceapp/glance/internal/config"
+	"github.com/glanceapp/glance/internal/server"
+)
+
+var (
+	Version = "dev"
+	Commit  = "none"
+	Date    = "unknown"
 )
 
 func main() {
-	os.Exit(glance.Main())
+	configPath := flag.String("config", "glance.yml", "Path to the configuration file")
+	showVersion := flag.Bool("version", false, "Print version information and exit")
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Printf("glance %s (commit: %s, built: %s)\n", Version, Commit, Date)
+		os.Exit(0)
+	}
+
+	cfg, err := config.Load(*configPath)
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+	}
+
+	srv, err := server.New(cfg)
+	if err != nil {
+		log.Fatalf("Failed to initialize server: %v", err)
+	}
+
+	log.Printf("Starting glance %s on %s:%d", Version, cfg.Server.Host, cfg.Server.Port)
+
+	if err := srv.Start(); err != nil {
+		log.Fatalf("Server error: %v", err)
+	}
 }
